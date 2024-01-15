aedacfd1d54e491ba5972a4d765f7b00c62d8b89
saj1k2k <jonikbarezzi3@gmail.com>
2024-01-15 14:22:31 +0300

    feat: add 2 protected routes

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { useAuth } from "../hooks/useAuth";
+import img from "../../public/shield-with-lock-svgrepo-com.svg";
+interface Props {
+  children: JSX.Element;
+}
+
+const ProtectedRoute: FC<Props> = ({ children }) => {
+  const isAuth = useAuth();
+  return (
+    <>
+      {isAuth ? (
+        children
+      ) : (
+        <div className="mt-20 flex-col items-center justify-center gap-10 flex">
+          <h1 className="text-2xl">To view this page you mast be logged in</h1>
+          <img className="w-1/3" src={img} alt="lock" />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ProtectedRoute;
